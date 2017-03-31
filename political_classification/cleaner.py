c8024dfd88913200aa3720985e705d8c11b80387
mikz <mikias@outlook.com>
2017-03-31 13:18:01 -0400

    classify labeling

--- political_classification/cleaner.py
+++ political_classification/cleaner.py
@@ -1,46 +1,34 @@
 import json
 import sys
+import os
 from pprint import pprint
-from ../constants.py import Classifications
+path = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
+sys.path.append(path)
+import constants
 
-#get tweets for account X
-#practice tweets
 tweet_list = [{"text": "this is a democratic tweet"}, {"text": "this is another democratic tweet"}]
 
-label = Classifications.democrat
+label = constants.Classifications.democrat
 
 fname = 'labelled.json'
+if not os.path.isfile(fname):
+    with open(fname, mode='w', encoding='utf-8') as f:
+        json.dump([], f)
 
-f = open(fname, 'a')
+with open(fname, mode='r', encoding='utf-8') as feedsjson:
+    feeds = json.load(feedsjson) 
 
 for tweet in tweet_list:
-	data = {}
-	data['text'] = tweet['text']
-	data['label'] = label
-	try:
-		json.dump(data, f)
-	except IOError as e:
-            pprint ("I/O error({0}): {1}".format(e.errno, e.strerror))
+    entry = {'text': tweet['text'], 'label':label.value}
+    feeds.append(entry)
+    
+with open(fname, mode='w', encoding='utf-8') as feedsjson:
+    try:
+        json.dump(feeds, feedsjson)
+    except IOError as e:
+        pprint ("I/O error({0}): {1}".format(e.errno, e.strerror))
     except ValueError:
-            pprint ("Could not convert data to an integer.")
+        pprint ("Could not convert data to an integer.")
     except:
-            print ("Unexpected error:", sys.exc_info()[0])
-            raise
-	
-
-# with open(first_arg) as data_file:
-    # try:
-        # data = json.load(data_file)
-        # pprint(data['text'])
-        # json_str = json.dumps(data)
-        # with open(first_arg, 'w') as f:
-			# jsonobj
-            # json.dump(data['text'], f) 
-            # #to make handling the file size eaiser rewrite the "text" within the orignal json file
-    # except IOError as e:
-            # pprint ("I/O error({0}): {1}".format(e.errno, e.strerror))
-    # except ValueError:
-            # pprint ("Could not convert data to an integer.")
-    # except:
-            # print ("Unexpected error:", sys.exc_info()[0])
-            # raise
+        print ("Unexpected error:", sys.exc_info()[0])
+        raise
