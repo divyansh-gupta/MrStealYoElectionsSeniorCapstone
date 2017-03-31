473b60bdbfb9a56db36c4f571e0f8c509995a14e
mikz <mikias@outlook.com>
2017-03-31 13:49:28 -0400

    remove unwanted information

--- political_classification/cleaner.py
+++ political_classification/cleaner.py
@@ -1,6 +1,7 @@
 import json
 import sys
 import os
+import time
 from pprint import pprint
 path = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
 sys.path.append(path)
@@ -8,22 +9,20 @@ import constants
 
 
 def get_tweets(screen_name):
-	alltweets = []
-	new_tweets = api.user_timeline(screen_name = screen_name, count=200)
-	alltweets.extend(new_tweets)
-	oldest = alltweets[-1].id - 1
-	
-	while len(new_tweets) > 0:
-		new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
-		alltweets.extend(new_tweets)
-		oldest = alltweets[-1].id - 1
-
-
-
+    alltweets = []
+    new_tweets = constants.api.user_timeline(screen_name = screen_name, count=200)
+    alltweets.extend(new_tweets)
+    oldest = alltweets[-1].id - 1
+    while len(new_tweets) > 0:
+            new_tweets = constants.api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+            alltweets.extend(new_tweets)
+            oldest = alltweets[-1].id - 1
+            time.sleep(2)
+    return alltweets
 tweet_list = get_tweets('BernieSanders')
 
-pprint(tweet_list[0])
-exit()
+#pprint(tweet_list[0]._json['text'])
+#exit()
 
 label = constants.Classifications.democrat
 
@@ -36,7 +35,7 @@ with open(fname, mode='r', encoding='utf-8') as feedsjson:
     feeds = json.load(feedsjson) 
 
 for tweet in tweet_list:
-    entry = {'text': tweet['text'], 'label':label.value}
+    entry = {'text': tweet._json['text'], 'label':label.value}
     feeds.append(entry)
     
 with open(fname, mode='w', encoding='utf-8') as feedsjson:
