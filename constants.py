0a0ad75dbd49197909f07520f0b9dcf7fc53d487
mikz <mikias@outlook.com>
2017-03-31 13:23:44 -0400

    added api key to constants

--- constants.py
+++ constants.py
@@ -1,5 +1,9 @@
 from enum import Enum
+import tweepy
 class Classifications(Enum):
 	democrat = 1
 	republican = 2
 	third = 3
+auth = tweepy.OAuthHandler("***REMOVED***", "***REMOVED***")
+auth.set_access_token("***REMOVED***", "***REMOVED***")
+api = tweepy.API(auth)
