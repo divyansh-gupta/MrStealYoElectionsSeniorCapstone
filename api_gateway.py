c88ddcf4c019da671af658288721ae033a72800f
divyansh-gupta <divyg@vt.edu>
2017-04-18 03:15:12 -0400

    Added API gateway task scheduler

---
+++ api_gateway.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+api_gateway_key = '***REMOVED***'
+
+def queue_task(url, queue, message_body):
+	r = requests.post(url, data = json.dumps({ 'queue': queue, 'message_body': message_body }),
+		headers = {'x-api-key': api_gateway_key })
+	print(r.status_code, r.reason, r.text)
+
+def queue_2012_tasks(start_inclusive, end_exclusive):
+	for i in range(start_inclusive, end_exclusive):
+		queue_task("***REMOVED***", "2012.fifo", i)
+
+queue_2012_tasks(0, 10)
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ git clone "https://divyansh-gupta:***REMOVED***@github.com/zakkl13/sa_capstone_e
 sudo apt-get -y update;
 sudo apt-get install -y python3-pip;
 sudo apt-get install -y python-mysqldb libmysqlclient-dev;
-sudo pip3 install peewee pytz textblob tweepy boto3 Cython mysqlclient;
+sudo pip3 install peewee pytz textblob tweepy boto3 Cython mysqlclient requests;
 mkdir ~/.aws;
 touch ~/.aws/config;
 touch ~/.aws/credentials;
@@ -11,4 +11,4 @@ sudo chmod 777 ~/.aws/*;
 sudo printf "[default]\naws_access_key_id = ***REMOVED***\naws_secret_access_key = ***REMOVED***" > ~/.aws/credentials
 sudo printf "[default]\nregion=us-east-1" > ~/.aws/config;
 cd sa_capstone_elections_project;
-python3 cythondriver.py;
+python3 2012_consumer.py;
